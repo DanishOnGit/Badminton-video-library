54e74fdb4a85c6358b6232d5e29db22074328749
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-12 02:12:22 +0530

    database updated

--- src/Database/allVideos.js
+++ src/Database/allVideos.js
@@ -26,24 +26,24 @@ avatar:"https://i.postimg.cc/hhr25Svj/salman-hossain-saif-n-Lerr-P856-o-unsplash
         avatar:"https://i.postimg.cc/hhr25Svj/salman-hossain-saif-n-Lerr-P856-o-unsplash.jpg"
           },
           {
-            id:"v=MsHMCZlcrXM",
-            videoTitle:"BASIC BADMINTON FOR BEGINNERS - PART 1 OF 3",
+            id:"v=IidZFd6vCJk",
+            videoTitle:"Badminton: Basic FOOTWORK - 6 corners",
             channelName:"Shuttle Life",
             level:"Beginner",
             thumbnail:"https://i.postimg.cc/KvwSPfLg/salman-hossain-saif-24z-MNig-Ma5-A-unsplash.jpg",
             avatar:"https://i.postimg.cc/hhr25Svj/salman-hossain-saif-n-Lerr-P856-o-unsplash.jpg"
               },
               {
-                id:"v=MsHMCZlcrXM",
-                videoTitle:"BASIC BADMINTON FOR BEGINNERS - PART 1 OF 3",
+                id:"v=pB8dX6InWyM",
+                videoTitle:"REAR / BACK COURT, BASIC FOOTWORK ",
                 channelName:"Shuttle Life",
                 level:"Beginner",
                 thumbnail:"https://i.postimg.cc/KvwSPfLg/salman-hossain-saif-24z-MNig-Ma5-A-unsplash.jpg",
                 avatar:"https://i.postimg.cc/hhr25Svj/salman-hossain-saif-n-Lerr-P856-o-unsplash.jpg"
                   },
                   {
-                    id:"v=MsHMCZlcrXM",
-                    videoTitle:"BASIC BADMINTON FOR BEGINNERS - PART 1 OF 3",
+                    id:"v=cP6f2zY8ma4",
+                    videoTitle:"FOOTWORK #12 - BASIC FOREHAND AND BACKHAND",
                     channelName:"Shuttle Life",
                     level:"Beginner",
                     thumbnail:"https://i.postimg.cc/KvwSPfLg/salman-hossain-saif-24z-MNig-Ma5-A-unsplash.jpg",
