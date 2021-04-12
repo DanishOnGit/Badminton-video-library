79656cecf9acd0dc8970370be5e54a0341161ab3
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-13 00:05:17 +0530

    refactor: some comments removed

--- src/Components/ReactPlayer.js
+++ src/Components/ReactPlayer.js
@@ -1,9 +1,44 @@
 import { useParams } from "react-router-dom";
-import ReactPlayer from 'react-player/youtube';
-
-
-export const VideoPlayer=()=>{
+import ReactPlayer from "react-player/youtube";
+import {allVideos} from "../Database"
+import { useVideo } from "../Contexts";
+import {checkIfAlreadyPresent} from "../Utilities"
+export const VideoPlayer = () => {
   
-const {videoId}=useParams();
-  return <ReactPlayer controls={true}  url={`https://www.youtube.com/watch?${videoId}`} />
-}
+  const { videoId } = useParams();
+  const {state:{likedVideos,watchLaterVideos,historyVideos},dispatch}=useVideo();
+
+  const videoDetails=allVideos.find(item=>item.id===videoId)
+
+  // dispatch({type:"ADD_TO_HISTORY",payload:videoDetails})
+   
+  return (
+    <>
+    <div className="video-player-wrapper">
+      <ReactPlayer onStart={()=>!checkIfAlreadyPresent(historyVideos,videoId)?dispatch({type:"ADD_TO_HISTORY",payload:videoDetails}):null}
+        controls={true}
+        url={`https://www.youtube.com/watch?${videoId}`}
+      />
+      <div className="interactions-wrapper">
+
+        <ul className="list-items-flex list-non-bullet spaced">
+          <li>
+            <i onClick={()=>!checkIfAlreadyPresent(likedVideos,videoId)?dispatch({type:"LIKE_VIDEO",payload:videoDetails}):null} className="fas fa-thumbs-up pointer"></i>
+          </li>
+          <li>
+            <i className="fas fa-list pointer"></i>
+          </li>
+          <li>
+            <i onClick={()=>!checkIfAlreadyPresent(watchLaterVideos,videoId)?dispatch({type:"ADD_TO_WATCH_LATER",payload:videoDetails}):null} className="fas fa-clock pointer"></i>
+          </li>
+        </ul>
+
+        <ul className="interactions-wrapper__note list-non-bullet">
+          <li className="pointer"><i className="fas fa-pen pointer"></i> Add Notes</li>
+        </ul>
+
+      </div>
+      </div>
+    </>
+  );
+};
--- src/Contexts/VideoContext.js
+++ src/Contexts/VideoContext.js
@@ -1,24 +1,31 @@
 import { createContext, useContext, useReducer } from "react";
 // import {reducer} from "./Contexts";
-import ReactPlayer from 'react-player/youtube'
 
-import {Link} from "react-router-dom";
 const VideoContext=createContext();
 
 export const VideoProvider= ({children})=> {
 
   const reducer=(state,action)=>{
     switch(action.type){
-      case "SHOW_VIDEO":{
-        return <Link to="https://www.youtube.com/watch?v=MsHMCZlcrXM&ab_channel=ShuttleLife" ></Link>
-        
+      case "LIKE_VIDEO":{
+       return {...state,likedVideos:[...state.likedVideos,{...action.payload,existsInLikedVideos:true}]}
       }
+      case "ADD_TO_WATCH_LATER":{
+        return {...state,watchLaterVideos:[...state.watchLaterVideos,{...action.payload,existsInWatchLaterVideos:true}]}
+       }
+      
+       case "ADD_TO_HISTORY":{
+         return {...state,historyVideos:[...state.historyVideos,{...action.payload,existsInHistory:true}]}
+        }
       default:
           return state
     }
   } 
+  const initialState={likedVideos:[],
+  watchLaterVideos:[],
+historyVideos:[]}
 
-const [state,dispatch]=useReducer(reducer,{videoId:""})
+const [state,dispatch]=useReducer(reducer,initialState)
 return (<VideoContext.Provider value={{state,dispatch}}>
   {children}
   </VideoContext.Provider>)
